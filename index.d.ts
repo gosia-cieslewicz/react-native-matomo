b1a8efe1561e9b1d6959083c132476225d75faa5
florin-p <92801911+florin-p@users.noreply.github.com>
2022-01-13 13:28:10 +0100

    WTB-892 intergation with rn app (#1)
    * added ts description file
    
    * added ts description file

---
+++ index.d.ts
@@ -0,0 +1,28 @@
+declare module "react-native-matomo" {
+    namespace BNFMatomo {
+        export function initTracker(apiUrl: string, siteId: number);
+
+        export function setUserId(userId: string);
+
+        export function setCustomDimension(id: string, value: string);
+
+        export function setAppOptOut(isOptedOut: boolean);
+
+        export function trackAppDownload(path: string, title: string);
+
+        export function trackGoal(goalId: string, revenue: any);
+
+        export function trackEvent(category: string, action: string, name: string, value: number, url?: string);
+
+        export function trackCampaign(name: string, keyword: string);
+
+        export function trackScreen(screen: string, title: string);
+
+        export function trackContentImpression(name: string, piece: string, target: string);
+
+        export function trackContentInteraction(name: string, interaction: string, piece: string, target: string);
+
+        export function trackSearch(query: string, category: string, resultCount: string, url: string);
+    }
+    export default BNFMatomo;
+}
