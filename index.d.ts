dc975c54ccbaa4aabc968c5da65589e767ebcd4b
florin-p <92801911+florin-p@users.noreply.github.com>
2022-01-25 15:21:44 +0100

    update setCustomDimensions def (#4)

--- index.d.ts
+++ index.d.ts
@@ -4,7 +4,7 @@ declare module "react-native-matomo" {
 
         export function setUserId(userId: string);
 
-        export function setCustomDimension(id: string, value: string);
+        export function setCustomDimension(id: number, value: string);
 
         export function setAppOptOut(isOptedOut: boolean);
 
